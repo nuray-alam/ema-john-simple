b8a7f411c5096a4c03024c5fc85f072957b175dd
Md. Nuray Alam Siddik <siddik4x@gmail.com>
2021-03-01 01:18:51 +0600

    shop, product, header component and fontawesome added

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import Product from '../Product/Product';
+import './Shop.css'
+const Shop = () => {
+    console.log(fakeData);
+    const first10 = fakeData.slice(0, 10);
+    const [products, setProducts] = useState(first10);
+
+    return (
+        <div className='shop-container'>
+            <div className="product-container">
+                {
+                    products.map(product => <Product key = {product.key} product={product}></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <h1>This is cart</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
